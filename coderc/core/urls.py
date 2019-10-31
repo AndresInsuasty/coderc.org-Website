560b7059bd47ef337914b94f2477ee6504db339f
Andres <andresinsuastyd10@gmail.com>
2019-10-31 15:09:13 -0500

    Replica de pagina coderc.org listo Frontend

--- coderc/core/urls.py
+++ coderc/core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name="home"),
+    path('asesoria',views.asesoria,name="asesoria"),
+    path('capacitacion',views.capacitacion,name="capacitacion"),
+    path('blog',views.blog,name="blog"),
+    path('about',views.about,name="about"),
+    path('contacto',views.contacto,name="contacto")
 ]
--- coderc/core/views.py
+++ coderc/core/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "core/index.html")
+
+def asesoria(request):
+    return render(request,"core/asesoria.html")
+
+def capacitacion(request):
+    return render(request,"core/capacitacion.html")
+
+def blog(request):
+    return render(request,"core/blog.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def contacto(request):
+    return render(request, "core/contacto.html")
