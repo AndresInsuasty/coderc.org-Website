18aa4212973a9cc2ca5a0e3fe31b4ae0fbb8e304
Andres <andresinsuastyd10@gmail.com>
2019-10-31 23:08:22 -0500

    Modelo Comproagro listo, falta pulir presentacion

--- coderc/comproagro/views.py
+++ coderc/comproagro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def comproagro(request):
-    return render(request,"comproagro/comproagro.html")
+    projects = Project.objects.all()
+    return render(request,"comproagro/comproagro.html",{'projects':projects})
